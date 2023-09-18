8c2c7f910f9000efff272df8a5ee7df65bcbd0e9
purnell russell <purnell.russell@sciacademy.org>
2023-09-18 15:19:40 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -86,6 +86,7 @@ $(function () {
     createCannon("top", 550,890)
     createCannon("top", 850,890)
     createCannon("top", 1250,890)
+    createCannon("right",550,890,)
 
 
     /////////////////////////////////////////////////
